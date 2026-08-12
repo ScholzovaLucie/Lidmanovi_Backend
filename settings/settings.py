b2e58fef0235a5fe6718b2daa67e71fa75919d13
Novakova <99414133+Scholzova@users.noreply.github.com>
2026-08-12 07:37:25 +0200

    update

--- settings/settings.py
+++ settings/settings.py
@@ -101,12 +101,18 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
-        'PORT': 5432,
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': os.environ.get('POSTGRES_DB'),
+        #'USER': os.environ.get('POSTGRES_USER'),
+        #'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        #'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        #'PORT': 5432,
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
